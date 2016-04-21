8680850d30bf3fbe4b250b2ce443024368b2f4a8
crossjs <liwenfu@crossjs.com>
2016-04-21 15:57:07 +0800

    revert routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,26 +5,22 @@ export default {
     name: 'home',
     skip: true,
     exact: true,
-    // component: resolve => require(['views/Home'], resolve)
-    component: require('views/Home')
+    component: resolve => require(['views/Home'], resolve)
   },
   '/about': {
     icon: 'about',
     title: '关于',
     name: 'about',
-    // component: resolve => require(['views/About'], resolve)
-    component: require('views/About')
+    component: resolve => require(['views/About'], resolve)
   },
   '/users': {
     icon: 'users',
     title: '用户',
     name: 'users',
     auth: true,
-    // component: resolve => require(['views/Users'], resolve),
     component: resolve => require(['views/Users'], resolve),
     subRoutes: {
       '/:username': {
-        // component: resolve => require(['views/Users'], resolve)
         component: resolve => require(['views/Users'], resolve)
       }
     }
@@ -34,29 +30,25 @@ export default {
     title: '资料',
     name: 'profile',
     auth: true,
-    // component: resolve => require(['views/Profile'], resolve)
-    component: require('views/Profile')
+    component: resolve => require(['views/Profile'], resolve)
   },
   '/signup': {
     icon: 'signup',
     title: '注册',
     name: 'signup',
-    // component: resolve => require(['views/Signup'], resolve)
-    component: require('views/Signup')
+    component: resolve => require(['views/Signup'], resolve)
   },
   '/login': {
     icon: 'login',
     title: '登录',
     name: 'login',
-    // component: resolve => require(['views/Login'], resolve)
-    component: require('views/Login')
+    component: resolve => require(['views/Login'], resolve)
   },
   '/logout': {
     icon: 'logout',
     title: '退出',
     name: 'logout',
     auth: true,
-    // component: resolve => require(['views/Logout'], resolve)
-    component: require('views/Logout')
+    component: resolve => require(['views/Logout'], resolve)
   }
 }
