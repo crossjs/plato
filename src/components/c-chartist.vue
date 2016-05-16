bb30dc1262424a1b5508e15274b4a3cff1006f7b
crossjs <liwenfu@crossjs.com>
2016-05-17 01:37:00 +0800

    add chartist

---
+++ src/components/c-chartist.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="c-chartist"
+    :class="[cls, ratio, noData]">{{message}}</div>
+</template>
+
+<script>
+import Chartist from 'chartist'
+export default {
+  props: {
+    cls: {
+      type: String,
+      default: ''
+    },
+    ratio: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      required: true,
+      validator (val) {
+        return val === 'Pie' || val === 'Line' || val === 'Bar'
+      }
+    },
+    data: {
+      type: Object
+    },
+    options: {
+      type: Object
+    },
+    responsive: {
+      type: Array
+    },
+    events: {
+      type: Object
+    }
+  },
+
+  ready () {
+    this.draw()
+  },
+
+  data () {
+    return {
+      error: {
+        onError: false,
+        message: ''
+      },
+      noData: '',
+      message: ''
+    }
+  },
+
+  methods: {
+    draw () {
+      if (this.data) {
+        // data is empty
+        if (this.data.series.length < 1 || (this.type !== 'Pie' && this.data.labels.length < 1)) {
+          // clear the potential old chart
+          /* eslint no-new:0 */
+          new Chartist[this.type](this.$el, this.data, this.options, this.responsive)
+          this.setNoData()
+        // data is defined
+        } else {
+          this.noData = '' // remove class ct-nodata
+          this.message = '' // remove message no data
+          // clear error
+          if (this.error.onError) this.error = { onError: false, message: '' }
+          const chart = new Chartist[this.type](this.$el, this.data, this.options, this.responsive)
+          if (this.events) {
+            Object.keys(this.events).forEach(key => {
+              chart.on(key, this.events[key])
+            })
+          }
+        }
+      } else {
+        this.setNoData()
+      }
+    },
+    setNoData () {
+      this.error = {
+        onError: true,
+        message: ''
+      }
+      this.noData = 'ct-nodata'
+      this.message = this.error.message
+    }
+  },
+
+  watch: {
+    'ratio': 'draw',
+    'options': 'draw',
+    'data': {
+      handler: 'draw',
+      deep: true
+    },
+    'type': 'draw'
+  }
+}
+</script>
+
+<style src="chartist/dist/chartist.css"></style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,7 +1,95 @@
 <template>
   <div class="view-home">
-    <pre><code>
-    {{auth | json}}
-    </code></pre>
+    <chartist
+      v-for="chart of charts"
+      :ratio="chart.ratio"
+      :type="chart.type"
+      :data="chart.data"
+      :options="chart.options"
+      :responsive="chart.responsive"
+      :events="chart.events"></chartist>
   </div>
 </template>
+
+<script>
+import { Svg } from 'chartist'
+import Chartist from 'components/c-chartist'
+export default {
+  data () {
+    return {
+      charts: [{
+        type: 'Line',
+        data: {
+          labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+          series: [
+            [1, 5, 2, 5, 4, 3],
+            [2, 3, 4, 8, 1, 2],
+            [5, 4, 3, 2, 1, 0.5]
+          ]
+        },
+        options: {
+          // lineSmooth: Interpolation.step(),
+          axisX: {
+            labelInterpolationFnc (value) {
+              return 'W ' + value
+            }
+          }
+        },
+        events: {
+          draw (data) {
+            if (data.type === 'line' || data.type === 'area') {
+              data.element.animate({
+                d: {
+                  begin: 2000 * data.index,
+                  dur: 2000,
+                  from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
+                  to: data.path.clone().stringify(),
+                  easing: Svg.Easing.easeOutQuint
+                }
+              })
+            }
+          }
+        }
+      }, {
+        type: 'Bar',
+        data: {
+          labels: ['First quarter of the year', 'Second quarter of the year', 'Third quarter of the year', 'Fourth quarter of the year'],
+          series: [
+            [60000, 40000, 80000, 70000],
+            [40000, 30000, 70000, 65000],
+            [8000, 3000, 10000, 6000]
+          ]
+        },
+        options: {
+          seriesBarDistance: 10,
+          axisX: {
+            offset: 60
+          },
+          axisY: {
+            offset: 80,
+            labelInterpolationFnc (value) {
+              return value + ' CHF'
+            },
+            scaleMinSpace: 15
+          }
+        }
+      }, {
+        ratio: 'ct-square',
+        type: 'Pie',
+        data: {
+          series: [20, 10, 30, 40]
+        },
+        options: {
+          labelInterpolationFnc (value) {
+            return value + '%'
+          }
+        }
+      }]
+    }
+  },
+
+  components: {
+    Chartist
+  }
+}
+</script>
