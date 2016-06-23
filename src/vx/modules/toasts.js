f26f5f339e71b8d6787161255fbf9c11aee72881
crossjs <liwenfu@crossjs.com>
2016-06-23 22:27:17 +0800

    * :fish: update modules/toasts

--- src/vx/modules/toasts.js
+++ src/vx/modules/toasts.js
@@ -3,21 +3,14 @@ import {
   DELETE_TOAST
 } from '../types'
 
-import {
-  PROMISE_SUCCESS,
-  PROMISE_FAILURE
-} from '../constants'
-
 const state = {
   toasts: []
 }
 
 const mutations = {
   [ADD_TOAST] (state, { payload, meta }) {
-    if (meta === PROMISE_SUCCESS || meta === PROMISE_FAILURE) {
-      payload._id = Date.now()
-      state.toasts.push(payload)
-    }
+    payload._id = Date.now()
+    state.toasts.push(payload)
   },
 
   [DELETE_TOAST] (state, { payload }) {
