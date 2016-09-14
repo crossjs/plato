e898386865bf82ca5e50e681638975305539710d
crossjs <liwenfu@crossjs.com>
2016-09-15 01:33:20 +0800

    update e2e

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -5,10 +5,10 @@ module.exports = {
   'default e2e tests' (browser) {
     browser
     .url('http://localhost:3000')
-      .waitForElementVisible('.container', 5000)
-      .assert.elementPresent('.logo')
+      .waitForElementVisible('#container', 5000)
+      .assert.elementPresent('#logo')
       .assert.containsText('a', 'PLATO')
-      .assert.containsText('.history', 'back')
+      .assert.containsText('#history', 'back')
       // there is no router-view tag in vue 2
       .assert.elementCount('.router-view', 0)
       .end()
