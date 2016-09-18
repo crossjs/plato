2d2d118582e3760c672c8e20466d5ceb33e3535b
crossjs <liwenfu@crossjs.com>
2016-09-18 15:52:21 +0800

    update styles

--- src/router.js
+++ src/router.js
@@ -7,6 +7,29 @@ Vue.use(Router)
 
 const router = new Router({
   mode: 'hash',
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      // savedPosition is only available for popstate navigations.
+      return savedPosition
+    } else {
+      const position = {}
+      // new navigation.
+      // scroll to anchor by returning the selector
+      if (to.hash) {
+        position.selector = to.hash
+      }
+      // check if any matched route config has meta that requires scrolling to top
+      if (to.matched.some(m => m.meta.scrollToTop)) {
+        // cords will be used if no selector is provided,
+        // or if the selector didn't match any element.
+        position.x = 0
+        position.y = 0
+      }
+      // if the returned position is falsy or an empty object,
+      // will retain current scroll position.
+      return position
+    }
+  },
   routes,
   linkActiveClass: 'link-active'
 })
@@ -22,7 +45,7 @@ router.beforeEach((route, redirect, next) => {
 
 router.afterEach((/* route, redirect, next */) => {
   store.dispatch('setProgress', 100)
-  window.scrollTo(0, 0)
+  // window.scrollTo(0, 0)
 })
 
 export default router
