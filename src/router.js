df6a8a0323c50337bf624791e13a877376e8e37a
crossjs <liwenfu@crossjs.com>
2016-09-18 16:07:03 +0800

    use default linkActiveClass

--- src/router.js
+++ src/router.js
@@ -5,34 +5,7 @@ import routes from 'routes'
 
 Vue.use(Router)
 
-const router = new Router({
-  mode: 'hash',
-  scrollBehavior (to, from, savedPosition) {
-    if (savedPosition) {
-      // savedPosition is only available for popstate navigations.
-      return savedPosition
-    } else {
-      const position = {}
-      // new navigation.
-      // scroll to anchor by returning the selector
-      if (to.hash) {
-        position.selector = to.hash
-      }
-      // check if any matched route config has meta that requires scrolling to top
-      if (to.matched.some(m => m.meta.scrollToTop)) {
-        // cords will be used if no selector is provided,
-        // or if the selector didn't match any element.
-        position.x = 0
-        position.y = 0
-      }
-      // if the returned position is falsy or an empty object,
-      // will retain current scroll position.
-      return position
-    }
-  },
-  routes,
-  linkActiveClass: 'link-active'
-})
+const router = new Router({ routes })
 
 router.beforeEach((route, redirect, next) => {
   store.dispatch('setProgress', 80)
@@ -43,9 +16,8 @@ router.beforeEach((route, redirect, next) => {
   }
 })
 
-router.afterEach((/* route, redirect, next */) => {
+router.afterEach(() => {
   store.dispatch('setProgress', 100)
-  // window.scrollTo(0, 0)
 })
 
 export default router
