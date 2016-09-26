96fc5931e06b377cada9fa36ad4360b6f1e47891
crossjs <liwenfu@crossjs.com>
2016-09-26 19:22:13 +0800

    iconfont fallback

--- src/demos/icon.vue
+++ src/demos/icon.vue
@@ -1,30 +1,30 @@
 <template>
-  <div class="fs-64">
-    <c-pane>
-      <c-icon>down</c-icon>
-      <c-icon>user</c-icon>
-      <c-icon>lock</c-icon>
-      <c-icon>menu</c-icon>
-      <c-icon>demo</c-icon>
-      <c-icon>chart</c-icon>
-      <c-icon>reload</c-icon>
-      <c-icon>back</c-icon>
-      <c-icon>view</c-icon>
-      <c-icon>github</c-icon>
-    </c-pane>
-    <c-pane>
-      <p>use ligatures in iconfont, see <a href="https://en.wikipedia.org/wiki/Typographic_ligature">Typographic ligature</a></p>
-    </c-pane>
+  <div>
+    <c-row :flex="false">
+      <c-icon cls="fs-64" v-for="(value, key) in maps">{{key}}</c-icon>
+      <p>use <a href="https://en.wikipedia.org/wiki/Typographic_ligature">ligatures</a> in iconfont, for modern browsers</p>
+    </c-row>
+    <c-row :flex="false">
+      <c-icon cls="fs-64" v-for="(value, key) in maps" v-html="value"></c-icon>
+      <p>use unicode in iconfont, for old browsers</p>
+    </c-row>
   </div>
 </template>
 
 <script>
-import CPane from 'components/c-pane'
+import CRow from 'components/c-row'
 import CIcon from 'components/c-icon'
+import maps from 'styles/fonts/plato.maps'
 
 export default {
+  data () {
+    return {
+      maps
+    }
+  },
+
   components: {
-    CPane,
+    CRow,
     CIcon
   }
 }
