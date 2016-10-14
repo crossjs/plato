a900895096de44b545de8b6790a6f385de1816a1
crossjs <liwenfu@crossjs.com>
2016-10-14 16:08:33 +0800

    update uploader component

--- src/demos/upload.vue
+++ src/demos/upload.vue
@@ -2,7 +2,7 @@
   <div class="d-upload">
     <c-pane>
       <p>Simple Uploader</p>
-      <c-upload action="/upload" @change="change">
+      <c-upload action="/api/upload" @change="change">
         <div class="d-upload-container">
           <div class="d-upload-preview">
             <img v-if="src" :src="src" :alt="file.name" />
@@ -32,6 +32,7 @@ export default {
       src: undefined
     }
   },
+
   methods: {
     change (ret, file) {
       console.log('------------------------------')
@@ -68,65 +69,4 @@ export default {
 }
 </script>
 
-<style lang="postcss">
-.d-upload {
-  padding: dpr(5px);
-}
-.d-upload-container {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 2;
-}
-
-.d-upload-preview {
-  width: 100%;
-  height: 100%;
-
-  & > img {
-    width: 100%;
-    height: 100%;
-  }
-
-  & > p {
-    width: 100%;
-    height: 100%;
-    text-align: center;
-    text-transform: capitalize;
-  }
-}
-
-.d-upload-progress {
-  width: 100%;
-  height: 0;
-  z-index: 3;
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: rgba(56, 173, 255, 0.2);
-  transition: height 0.3s linear;
-}
-
-.d-upload-text {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  color: #fff;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.d-upload-icon {
-  width: 100%;
-  height: 100%;
-  text-align: center;
-}
-</style>
+<style src="styles/demo"></style>
