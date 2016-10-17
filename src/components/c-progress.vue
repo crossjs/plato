8203d96beafe795a99b2297e98c103a5f9a07f02
crossjs <liwenfu@crossjs.com>
2016-10-17 15:53:05 +0800

    bugfix progress in iOS

--- src/components/c-progress.vue
+++ src/components/c-progress.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="c-progress" :style="{'padding-left': '' + progress + '%'}"></div>
+  <div class="c-progress">
+    <div class="c-progress-content"
+      :style="{'transform': 'translateX(-' + (100 - progress) + '%)'}"></div>
+  </div>
 </template>
 
 <script>
@@ -10,8 +13,11 @@ export default {
 
   props: {
     progress: {
-      type: [String, Number],
-      default: 0
+      type: Number,
+      default: 0,
+      validator (val) {
+        return +val >= 0 && +val <= 100
+      }
     }
   }
 }
