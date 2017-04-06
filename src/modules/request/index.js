c6a60e8b02d5109b706de8e5d44b61a945298f26
crossjs <liwenfu@crossjs.com>
2017-04-06 22:25:40 +0800

    update modules/request

--- src/modules/request/index.js
+++ src/modules/request/index.js
@@ -1,4 +1,4 @@
-import { configure } from 'platojs/util/request'
+import { intercept } from 'platojs/util/request'
 
 /**
  * 修改 request 方法的全局配置
@@ -8,16 +8,15 @@ export default ({ name, version }, options = {}) => {
   options = { scope: 'request', ...options }
 
   // 直接修改，不需要注册，不需要在回调时再修改
-  configure({
-    // configure 除了 mutator 还支持其它参数
-    // 其中 mutator 特殊处理，多次添加则会多次执行，
-    // 其它参数直接 cheap-merge 到默认参数
-    mutator (options) {
+  intercept({
+    // 修改请求体，可以用于添加 Dispatcher、Proxy 等
+    // 比如，对于 IE8/9，可以将 PUT/PATCH 请求修改为 POST 请求
+    request: [({ req }) => {
       // 在请求头部加入自定义字段
       // 可以用于添加 Authorization 等
-      options.headers['Who-Am-I'] = `${name}@${version}`
-      return options
-    }
+      req.headers['Who-Am-I'] = `${name}@${version}`
+      return { req }
+    }]
   })
 
   // 不返回任何值
