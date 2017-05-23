10ba6cfa52cafa5175acb3efe3e3936abaffe3da
crossjs <liwenfu@crossjs.com>
2017-05-23 17:39:24 +0800

    update translations

--- src/modules/about/index.js
+++ src/modules/about/index.js
@@ -1,6 +1,5 @@
 import createRoutes from './create-routes'
 
-// 方法 1：直接 return 需要注册的资源
 export default (context, options = {}) => {
   options = { scope: 'about', prefix: 'about', ...options }
 
@@ -10,13 +9,3 @@ export default (context, options = {}) => {
     options
   }
 }
-
-// 方法 2：通过 register 回调
-// export default (context, options = {}, register) => {
-//   options = { scope: 'about', prefix: '/', ...options }
-//
-//   register({
-//     routes: createRoutes(context, options),
-//     options
-//   })
-// }
